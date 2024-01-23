7307a880474a8b32f1ab62792245133eb17584ff
Alex <lxnn.git@gmail.com>
2024-01-23 19:23:24 +0000

    Add multiple objects to scene

--- source/main.c
+++ source/main.c
@@ -9,19 +9,22 @@
 #include "ray.h"
 #include "hittable.h"
 #include "sphere.h"
+#include "list.h"
 
 
-RGB ray_color(Ray r) {
-    static const Sphere s = {.hit=sphere_hit, .center={0, 0, -1}, .radius=0.5};
-    Hit hit_record;
-    if (s.hit((Hittable *) &s, r, 0, INFINITY, &hit_record)) {
-        V3 n = v3_unit(v3_sub(ray_at(r, hit_record.time), s.center));
-        return (RGB) {0.5*(n.x + 1), 0.5*(n.y + 1), 0.5*(n.z + 1)};
+RGB ray_color(Ray ray, Hittable *world) {
+    Hit record;
+    if (world->hit(world, ray, 0, INFINITY, &record)) {
+        return (RGB) {
+            0.5*(record.normal.x + 1),
+            0.5*(record.normal.y + 1),
+            0.5*(record.normal.z + 1),
+        };
     }
     else {
-        double t = 0.5 * v3_unit(r.direction).y + 0.5;
+        double a = 0.5 * v3_unit(ray.direction).y + 0.5;
         RGB light_blue = {0.5, 0.7, 1.0};
-        return rgb_blend(WHITE, light_blue, t);
+        return rgb_blend(WHITE, light_blue, a);
     }
 }
 
@@ -30,6 +33,17 @@ int main() {
     size_t image_height = 400;
     size_t image_width = (size_t) (image_height * aspect_ratio);
     assert(image_width > 0);
+
+    Sphere spheres[] = {
+        {.hit=sphere_hit, .center={0, 0, -1}, .radius=0.5},
+        {.hit=sphere_hit, .center={0, -100.5, -1}, .radius=100},
+    };
+    Hittable *objects[] = {
+        (Hittable *) &spheres[0],
+        (Hittable *) &spheres[1],
+    };
+    List object_list = {.hit=list_hit, .objects=objects, .count=2};
+    Hittable *world = (Hittable *) &object_list;
 
     double focal_length = 1.0;
     double viewport_height = 2.0;
@@ -63,11 +77,11 @@ int main() {
                 v3_scale(pixel_dv, (double) i)
             );
             V3 pixel_center = v3_add(pixel_zero, pixel_offset);
-            Ray r = (Ray) {
+            Ray ray = (Ray) {
                 .origin = camera_center,
                 .direction = v3_sub(pixel_center, camera_center),
             };
-            image[i * image_width + j] = ray_color(r);
+            image[i * image_width + j] = ray_color(ray, world);
         }
     }
 
