85a98f292aeb7ff28bd4a3f8c7ccdc13cb90ba85
Alex <lxnn.git@gmail.com>
2024-01-23 17:21:42 +0000

    Simplify list data type by removing dynanism.
    YAGNI

--- source/list.c
+++ source/list.c
@@ -1,7 +1,5 @@
 #include "list.h"
 
-#include <stdlib.h>
-
 bool list_hit(Hittable *self, Ray r, double tmin, double tmax, Hit *record) {
     List *list = (List *) self;
     for (size_t i = 0; i < list->length; i++) {
@@ -11,22 +9,4 @@ bool list_hit(Hittable *self, Ray r, double tmin, double tmax, Hit *record) {
         }
     }
     return false;
-}
-
-void list_init(List *self, size_t capacity) {
-    self->length = 0;
-    self->capacity = capacity;
-    self->elements = calloc(capacity, sizeof(Hittable *));
-}
-
-void list_destroy(List *self) {
-    free(self->elements);
-}
-
-void list_append(List *self, Hittable *hittable) {
-    if (self->length == self->capacity) {
-        self->capacity *= 2;
-        self->elements = realloc(self->elements, self->capacity * sizeof(Hittable *));
-    }
-    self->elements[self->length++] = hittable;
 }
--- source/list.h
+++ source/list.h
@@ -9,12 +9,8 @@ struct List {
     struct Hittable;
     Hittable **elements;
     size_t length;
-    size_t capacity;
 };
 
 bool list_hit(Hittable *self, Ray r, double tmin, double tmax, Hit *record);
-void list_init(List *self, size_t capacity);
-void list_destroy(List *self);
-void list_append(List *self, Hittable *hittable);
 
 #endif // LIST_H
