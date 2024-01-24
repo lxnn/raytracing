1eda4d2d64a86af89e0bbe9384d34fecd33d9965
Alex <lxnn.git@gmail.com>
2024-01-24 14:51:51 +0000

    Add interval clamp function

--- source/interval.c
+++ source/interval.c
@@ -7,3 +7,9 @@ bool interval_contains(Interval interval, double value) {
 bool interval_surrounds(Interval interval, double value) {
     return value > interval.min && value < interval.max;
 }
+
+double interval_clamp(Interval interval, double value) {
+    if (value <= interval.min) return interval.min;
+    if (value >= interval.max) return interval.max;
+    return value;
+}
--- source/interval.h
+++ source/interval.h
@@ -8,5 +8,6 @@ struct Interval {double min, max;};
 
 bool interval_contains(Interval interval, double value);
 bool interval_surrounds(Interval interval, double value);
+double interval_clamp(Interval interval, double value);
 
 #endif // INTERVAL_H
