e40e853483df6c76433ae06508084f23a4eebf1b
Alex <lxnn.git@gmail.com>
2024-01-25 19:37:52 +0000

    Make more renders

--- source/main.c
+++ source/main.c
@@ -11,24 +11,26 @@
 
 
 int main() {
-    Lambertian diffuse_red_ = {
+    Metal silver_ = {
+        .scatter=metal_scatter,
+        .albedo={0.9, 0.9, 0.9},
+        .fuzz=0.1,
+    };
+    Metal diffuse_red_ = {
         .scatter=lambertian_scatter,
         .albedo={1, 0, 0},
-    };
-    Lambertian diffuse_blue_ = {
-        .scatter=lambertian_scatter,
-        .albedo={0, 0, 1},
+        .fuzz=0.05,
     };
     Lambertian diffuse_grey_ = {
         .scatter=lambertian_scatter,
         .albedo={0.5, 0.5, 0.5},
     };
 
+    Material *silver = (Material *) &silver_;
     Material *diffuse_red = (Material *) &diffuse_red_;
-    Material *diffuse_blue = (Material *) &diffuse_blue_;
     Material *diffuse_grey = (Material *) &diffuse_grey_;
 
-    Sphere blue_ball = {.hit=sphere_hit, .material=diffuse_blue, .center={-1, 1, -1}, .radius=1};
+    Sphere blue_ball = {.hit=sphere_hit, .material=silver, .center={-1, 1, -1}, .radius=1};
     Sphere red_ball = {.hit=sphere_hit, .material=diffuse_red, .center={+1, 1, -1}, .radius=1};
     Sphere ground = {.hit=sphere_hit, .material=diffuse_grey, .center={0, -500, -1}, .radius=500};
 
@@ -42,7 +44,7 @@ int main() {
         .center = {3, 1, 2},
         .lookat = {0, 1, -1},
         .up = {1, 1, 0},
-        .vfov = 90,
+        .vfov = 40,
         .samples_per_pixel = 100,
         .max_bounces = 50,
     };
